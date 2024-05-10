98d664defe0b80ed77a20753c89b0ff72f69d8e1
Kyle <kyle@kylerich.dev>
2024-05-10 17:50:36 -0400

    Update GeneralSettingsView.swift

--- DesktopAI/GeneralSettingsView.swift
+++ DesktopAI/GeneralSettingsView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 import SwiftData
 
 struct GeneralSettingsView: View {
+    @AppStorage("isStreamEnabled") private var isStreamEnabled: Bool = false
+
     var body: some View {
-        Text("General Settings")
+        Form {
+            Section(header: Text("General Settings")) {
+                Toggle(isOn: $isStreamEnabled) {
+                    Text("Streaming")
+                }
+                Text("Enable this to start streaming. Better known as the Typewritter effect")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding()
     }
 }
