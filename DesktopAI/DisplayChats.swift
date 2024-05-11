9620814ffc3aab5e27f6dee328144e146186a25c
Kyle <kyle@kylerich.dev>
2024-05-11 16:36:09 -0400

    Update DisplayChats.swift

--- DesktopAI/DisplayChats.swift
+++ DesktopAI/DisplayChats.swift
@@ -20,7 +20,7 @@ struct DisplayChats: View {
                     ForEach(selectedItem.chatHistory.sorted(by: { $0.timestamp < $1.timestamp }), id: \.content) { message in
                         Text(message.content)
                             .padding(10)
-                            .background(message.isFromAI ? Color.blue : Color.gray)
+                            .background(message.isFromAI ? Color(red: 0.2, green: 0.2, blue: 0.2) : Color.blue)
                             .foregroundColor(.white)
                             .cornerRadius(10)
                             .frame(maxWidth: .infinity, alignment: message.isFromAI ? .leading : .trailing)
