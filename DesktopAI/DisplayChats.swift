ceaace6a3e6b088e92e595a23f94d00e94d9f166
Kyle <kyle@kylerich.dev>
2024-05-10 21:54:44 -0400

    Update DisplayChats.swift

--- DesktopAI/DisplayChats.swift
+++ DesktopAI/DisplayChats.swift
@@ -5,19 +5,38 @@
 //  Created by Cole Legere on 5/10/24.
 //
 
+import Foundation
 import SwiftUI
 import SwiftData
+
+struct ChatMessage {
+    let content: String
+    let isFromAI: Bool
+}
 
 struct DisplayChats: View {
     let selectedItem: Item
     @State private var userMessage = ""
+
+    let chatMessages: [ChatMessage] = [
+        ChatMessage(content: "Hello", isFromAI: false),
+        ChatMessage(content: "How are you?", isFromAI: true),
+        ChatMessage(content: "I'm good, thanks!", isFromAI: false),
+        ChatMessage(content: "How about you?", isFromAI: true)
+    ]
     
     var body: some View {
         ZStack {
             VStack {
                 ScrollView {
-                    VStack {
-                        Spacer()
+                    ForEach(chatMessages, id: \.content) { message in
+                        Text(message.content)
+                            .padding(10)
+                            .background(message.isFromAI ? Color.gray : Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .frame(maxWidth: .infinity, alignment: message.isFromAI ? .trailing : .leading)
+                            .padding(.bottom, 5)
                     }
                 }
                 HStack {
